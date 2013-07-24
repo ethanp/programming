9e92365a1bd3f0d0bd98a41ae3fc100788a47dc3
ethanp <ethanp@utexas.edu>
2013-07-24 12:25:43 -0500

    upload scala-sbt-template

---
+++ Scala/scala-sbt-template/src/test/scala/samples/scalatest.scala
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2012 Damian Helme
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   
+   */
+import org.scalatest.FunSuite
+import scala.collection.mutable.Stack
+ 
+class ExampleSuite extends FunSuite {
+ 
+  test("pop is invoked on a non-empty stack") {
+ 
+    val stack = new Stack[Int]
+    stack.push(1)
+    stack.push(2)
+    val oldSize = stack.size
+    val result = stack.pop()
+    assert(result === 2)
+    assert(stack.size === oldSize - 1)
+  }
+ 
+  test("pop is invoked on an empty stack") {
+ 
+    val emptyStack = new Stack[Int]
+    intercept[NoSuchElementException] {
+      emptyStack.pop()
+    }
+    assert(emptyStack.isEmpty)
+  }
+}
