5b3250f1c63a1120851c03e61e57250655750cdb
ethanp <ethanp@utexas.edu>
2013-07-24 14:14:22 -0500

    successfully used my own HyperLogLogMonoids for estimating set-size

--- Scala/AlgebirdTrials/src/main/scala/none/ethan/algebird/TryHyperLogLogMonoid.scala
+++ Scala/AlgebirdTrials/src/main/scala/none/ethan/algebird/TryHyperLogLogMonoid.scala
@@ -1,16 +1,34 @@
 package none.ethan.algebird
 
 import com.twitter.algebird._
+import HyperLogLog._  // explicitly import implicit conversions
 
 object TryHyperLogLogMonoid extends App {
-  import HyperLogLog._  // explicitly import implicit conversions
+  import HLLMUtilityFuncs._
+  none.ethan.algebird.HyperLogLogMonoidTests.runTests
 
-  /* copy in some useful stuff from the HLL test-file */
-  val r = new java.util.Random
+  // approximate number of 1000 users
+  val userSet = (1 to 1000).toSet
+  printf(
+    "Exact would be %d, but HLL says ~%d users in this set\n",
+    userSet.size,
+    approxCount(userSet).toInt
+  )
 
+  // intersect with a "similar" set sum it up and see what happens
+  val userSet2 = (1 to 2000 by 2).toSet
+  val userSets = Seq[Set[Int]](userSet,userSet2)
+  printf(
+    "Exact would be %d, but HLL says ~%d users in this set-intersection\n",
+    userSet.intersect(userSet2).size,
+    approxIntersect(userSets).toInt
+  )
+}
+
+object HLLMUtilityFuncs {
   def exactCount[T](it : Iterable[T]) : Int = it.toSet.size
-  def approxCount[T <% Array[Byte]](bits : Int, it : Iterable[T]) = {
-    val hll = new HyperLogLogMonoid(bits)
+  def approxCount[T <% Array[Byte]](it : Iterable[T]) = {
+    val hll = new HyperLogLogMonoid(12)
     hll.sizeOf(hll.sum(it.map { hll(_) })).estimate.toDouble
   }
 
@@ -19,53 +37,57 @@ object TryHyperLogLogMonoid extends App {
   def exactIntersect[T](it : Seq[Iterable[T]]) : Int = {
     it.foldLeft(Set[T]()) { (old, newS) => old ++ (newS.toSet) }.size
   }
-  def approxIntersect[T <% Array[Byte]](bits : Int, it : Seq[Iterable[T]]) : Double = {
-    val hll = new HyperLogLogMonoid(bits)
+
+  def approxIntersect[T <% Array[Byte]](it : Seq[Iterable[T]]) : Double = {
+    val hll = new HyperLogLogMonoid(12)
     //Map each iterable to a HLL instance:
     val seqHlls = it.map { iter => hll.sum(iter.view.map { hll(_) }) }
     hll.intersectionSize(seqHlls).estimate.toDouble
   }
+}
 
-  def test(bits : Int) {
+/* these tests from from com.twitter.algebird.HyperLogLogTest.scala
+  * to make sure all is functioning normally
+  * used without permission, not my code, etc.
+  */
+object HyperLogLogMonoidTests {
+  import HLLMUtilityFuncs._
+
+  val r = new java.util.Random
+
+  def test : Boolean = {
     val data = (0 to 10000).map { i => r.nextInt(1000) }
     val exact = exactCount(data).toDouble
-    val res = scala.math.abs(exact - approxCount(bits, data)) / exact - (3.5 * aveErrorOf(bits))
-    println(res)
+    val actualVariance = scala.math.abs(exact - approxCount(data)) / exact
+    val reasonableVariance = (3.5 * aveErrorOf(12))
+    actualVariance < reasonableVariance
   }
-  def testLong(bits : Int) {
+
+  def testLong : Boolean = {
     val data = (0 to 10000).map { i => r.nextLong }
     val exact = exactCount(data).toDouble
-    val res = scala.math.abs(exact - approxCount(bits, data)) / exact - (3.5 * aveErrorOf(bits))
-    println(res)
+    val actualVariance = scala.math.abs(exact - approxCount(data)) / exact
+    val reasonableVariance = (3.5 * aveErrorOf(12))
+    actualVariance < reasonableVariance
   }
-  def testLongIntersection(bits : Int, sets : Int) {
+
+  def testLongIntersection(sets : Int) : Boolean = {
     val data : Seq[Iterable[Int]] = (0 until sets).map { idx =>
       (0 to 1000).map { i => r.nextInt(100) }
     }.toSeq
     val exact = exactIntersect(data)
     val errorMult = scala.math.pow(2.0, sets) - 1.0
-    val res = scala.math.abs(exact - approxIntersect(bits, data)) / exact - (errorMult * aveErrorOf(bits))
-    println(res)
+    val actualVariance = scala.math.abs(exact - approxIntersect(data)) / exact
+    val reasonableVariance = (errorMult * aveErrorOf(12))
+    actualVariance < reasonableVariance
   }
 
-  val userSet = (1 to 1000).toSet
-  val userSet2 = (1 to 2000 by 2).toSet
-  val userSets = Seq[Set[Int]](userSet,userSet2)
-
-  val approxUsers =
-    userSets map {
-      aUserSet => {
-        val h = new HyperLogLogMonoid(12)
-        aUserSet map {
-          userID =>
-            h(userID)
-        }
-      }
+  def runTests {
+    if (test && testLong && testLongIntersection(5))
+      println("tests Pass\n")
+    else {
+      println("at least one test FAILED!@");
+      sys.exit(1)
     }
-
-  test(12)
-  testLong(12)
-  testLongIntersection(12, 5)
-
-  // add in a "similar" set sum it up and see what happens
+  }
 }
