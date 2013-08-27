3a5e05e0358fc17ee9618024fee4ae2bf2a50c40
ethanp <ethanp@utexas.edu>
2013-08-26 20:48:10 -0500

    calculates subject totals from csv

--- Java/Minco_XL/src/xl.java
+++ Java/Minco_XL/src/xl.java
@@ -5,14 +5,14 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.*;
 
 /**
  * Ethan Petuchowski
@@ -26,25 +26,89 @@ import java.util.Date;
  * The first Row ("1") is number 0
  * The first Col ("A") is number 0
  */
+
+/** TODO
+ * Summarize Minco Weekly Summary
+ * Parse Args for Date Specs
+ *
+ */
 public class xl
 {
     public static void main(String[] args) throws IOException, InvalidFormatException {
+
+
+        /* initializations */
+
+        // subject to change
+        String[] subjectsArray = {"Arch", "AI", "OS", "C++"};
+        String csvFile = "Minco Week 5.csv";  // week# can be easily obtained from the Date object
+        // /Users/Ethan/Library/Application\ Support/Minco/CSV_Files/Documents/Minco/2013/Minco\ Week\ 5.csv
+
+        // might eventually change
+        String excelFile = "SpringCopy.xlsm";
+        Map<String, Integer> mincoLine = new HashMap<>(5);
+        mincoLine.put("Date", 0);
+        mincoLine.put("Minutes", 3);
+        mincoLine.put("Title", 4);
+
+        // not subject to change
         int lastRowNum, todayRowNum, osColNum;
         lastRowNum = todayRowNum = osColNum = 0;
-        Boolean todayFound  = false;
+        Boolean todayFound = false;
+        String line;
+        List<String> subjects = Arrays.asList(subjectsArray);
+        Map<String, Integer> subjectTotals = new HashMap<>(subjectsArray.length);
+        for (String subject : subjects) {
+            subjectTotals.put(subject, 0);
+        }
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd"); // bc of minco dates
+        String today = dateFormat.format(new Date());
+
+
+
+
+        /* read and calculate Minco Weekly Summary */
+
+        BufferedReader csv = new BufferedReader(new FileReader(csvFile));
+
+        // skip headers
+        if (csv.readLine() == null) {
+            System.out.println("This Minco File is empty or missing or something");
+            System.exit(1);
+        }
+
+        while ((line = csv.readLine()) != null) {
+            String[] splitLine = line.replaceAll("\"", "").replaceAll("\u0000", "").split(",");
+            System.out.println(splitLine[mincoLine.get("Date")]+" "+splitLine[mincoLine.get("Title")]);
+
+            // only use the correct date
+            if (){};
+
+            // first word of title
+            String thisSubject = splitLine[mincoLine.get("Title")].split("\\s+")[0];
+
+            System.out.println(thisSubject);
+            if (subjectTotals.containsKey(thisSubject)) {
+                String thisTimeString = splitLine[mincoLine.get("Minutes")];
+                int thisTime = Integer.parseInt(thisTimeString);
+                int oldTotal = subjectTotals.get(thisSubject);
+                subjectTotals.put(thisSubject, oldTotal + thisTime);
+            }
+        }
+
+        for (String s : subjectTotals.keySet()) {
+            System.out.println(s+" => "+subjectTotals.get(s));
+        }
+
+
+
+
+        /* read and update XL File */
 
         // open the file and worksheet
-        InputStream inp = new FileInputStream("SpringCopy.xlsm");
-        Workbook wb = WorkbookFactory.create(inp);
-        Sheet sheet = wb.getSheet("MainSheet");
-
-        // this is how we'll format dates
-        SimpleDateFormat formatter = new SimpleDateFormat("MMM/dd/yyyy");
-
-        // get the current date
-        Calendar currentDate = Calendar.getInstance();
-        String today = formatter.format(currentDate.getTime());
-        System.out.println(today);
+        InputStream inputStream = new FileInputStream(excelFile);
+        Workbook workbook = WorkbookFactory.create(inputStream);
+        Sheet sheet = workbook.getSheet("MainSheet");
 
         // find last row number & today's row number
         for (Row row : sheet) {
@@ -54,14 +118,16 @@ public class xl
 
             Cell dateCell = row.getCell(0);
             if (dateCell != null) {
-                dateCell.setCellType(Cell.CELL_TYPE_NUMERIC); // this was necessary for date (?)
-                Date date = dateCell.getDateCellValue();
-                String dateString = formatter.format(date);
+                dateCell.setCellType(Cell.CELL_TYPE_NUMERIC); // necessary for getDate
+                Date thisDate = dateCell.getDateCellValue();
+                String dateString = dateFormat.format(thisDate);
                 System.out.println(dateString);
                 lastRowNum++;
 
-                if (dateString.equalsIgnoreCase(today))
+                if (dateString.equalsIgnoreCase(today)) {
+                    System.out.println("Found Today's Date");
                     todayFound = true;
+                }
 
                 if (!todayFound)
                     todayRowNum++;
