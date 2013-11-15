d80566cc1a196dd94a93682d0ecbde960885d4a4
ethanp <ethanp@utexas.edu>
2013-11-15 10:53:18 -0600

    RNN-G: grammar_trainer: refactored, updated, debugging, etc.

--- StuffIWrote/Python/GrammarBrain/GrammarBrain/grammar_trainer.py
+++ StuffIWrote/Python/GrammarBrain/GrammarBrain/grammar_trainer.py
@@ -1,25 +1,44 @@
 ''' -- FIRST TRIAL OF GRAMMATICALITY CLASSIFIER (SRN w/ BPTT) -- '''
 from random import sample, random, shuffle
 from pybrain.utilities import percentError
+from numpy import argmax
 from get_brown_pos_sents import get_nice_sentences, construct_sentence_matrices, print_n_sentences
 import brown_pos_map as bpm
+import time
+from pybrain.datasets.classification import SequenceClassificationDataSet
 
 
 # length restrictions on input sentences
-MAX_LEN = 8
+MAX_LEN = 5
 MIN_LEN = 4
 
 # number of different part of speech categorizations
 NUM_REG_POS = len(bpm.pos_vector_map.keys())
 NUM_BROWN_POS = len(bpm.pos_map.keys())
 NUM_OUTPUTS = 2
-HIDDEN_LAYER_SIZE = 5
+HIDDEN_LAYER_SIZE = 50
 
+
+''' let's say [1] means GRAMMATICAL, and [0] means UNGRAMMATICAL '''
+def insert_grammatical_sequence(dataset, sentence_mat):
+    dataset.newSequence()
+    for i, word_vector in enumerate(sentence_mat):
+        if i < len(sentence_mat)-1:
+            dataset.appendLinked(word_vector, [0])
+        else:
+            dataset.appendLinked(word_vector, [1])
+
+
+def insert_randomized_sequence(dataset, sentence_mat):
+    dataset.newSequence()
+    dup_sent_mat = sentence_mat[:]
+    shuffle(dup_sent_mat)
+    for word_vector in dup_sent_mat:
+        dataset.appendLinked(word_vector, [0])
 
 def create_train_and_test_sets(MIN_LEN, MAX_LEN):
     # http://pybrain.org/docs/tutorial/datasets.html
     # http://pybrain.org/docs/api/datasets/classificationdataset.html
-    from pybrain.datasets.classification import SequenceClassificationDataSet
 
     def print_data_data(data, name):
         print "num", name, "patterns: ", len(data)
@@ -28,15 +47,15 @@ def create_train_and_test_sets(MIN_LEN, MAX_LEN):
         print data['input'][0], data['target'][0], data['class'][0]
 
     # inp: dimensionality of the input (I think this is the sentence length)
-    # number of targets (output dimensionality, I think? Maybe not...I'm not sure!)
+    # number of targets (output dimensionality [i.e. #outNeurons], I think?)
     # nb_classes: number of possible classifications (i.e. grammatical or not)
-    train_data = SequenceClassificationDataSet(inp=NUM_REG_POS, target=1, nb_classes=2)
-    test_data = SequenceClassificationDataSet(inp=NUM_REG_POS, target=1, nb_classes=2)
+    train_data = SequenceClassificationDataSet(inp=NUM_REG_POS, target=1)
+    test_data = SequenceClassificationDataSet(inp=NUM_REG_POS, target=1)
 
     # brown dataset, no mid-sentence punctuation, no numbers, ends in period, within length range
     sentences = get_nice_sentences(MAX_LEN, MIN_LEN)
     print '\ntotal number of sentences:', len(sentences)
-    print '\nFirst five sentences between length', MIN_LEN-1, 'and', MAX_LEN
+    print '\nFirst five sentences between length', MIN_LEN-1, 'and', MAX_LEN-2
     print '------------------------------------------------------------'
     print_n_sentences(sentences, n=5)
     print '------------------------------------------------------------'
@@ -44,38 +63,16 @@ def create_train_and_test_sets(MIN_LEN, MAX_LEN):
     sentence_matrices = construct_sentence_matrices(sentences)
 
     print 'creating training and test sets...'
-
     for sentence_matrix in sentence_matrices:
         if random() < .25:  # percent distribution between sets needn't be perfect, right?
-            # the grammatical one
-            test_data.newSequence()
-            for word_vector in sentence_matrix:
-                test_data.appendLinked(word_vector, [0])
+            insert_grammatical_sequence(test_data, sentence_matrix)
+            insert_randomized_sequence(test_data, sentence_matrix)
 
-            # the "ungrammatical" one
-            test_data.newSequence()
-            shuffle(sentence_matrix)
-            for word_vector in sentence_matrix:
-                test_data.appendLinked(word_vector, [1])
         else:
-            # the grammatical one
-            train_data.newSequence()
-            for word_vector in sentence_matrix:
-                train_data.appendLinked(word_vector, [0])
+            insert_grammatical_sequence(train_data, sentence_matrix)
+            insert_randomized_sequence(train_data, sentence_matrix)
 
-            # the "ungrammatical" one
-            train_data.newSequence()
-            shuffle(sentence_matrix)
-            for word_vector in sentence_matrix:
-                train_data.appendLinked(word_vector, [1])
-
-    # add a /really/ negative example for good measure
-    train_data.addSample([7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 0], [1])  # class [1]: ungrammatical
-    test_data.addSample([6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 0], [1])
-
-    # encode classes with one output neuron per class
-    # duplicates the original target and stores them in an integer field named 'class'
-    # http://pybrain.org/docs/tutorial/fnn.html
+    # encode classes with one output neuron per class: [ungrammatical, grammatical]
     test_data._convertToOneOfMany()
     train_data._convertToOneOfMany()
 
@@ -87,45 +84,71 @@ def create_train_and_test_sets(MIN_LEN, MAX_LEN):
 
 
 def build_sigmoid_network():
-    ''' -- CONSTRUCT THE NETWORK (SRN) -- '''
-    from pybrain.structure import SigmoidLayer
+    from pybrain.structure import TanhLayer, LSTMLayer
     # TODO checkout the SharedFullConnection, LSTMLayer, BidirectionalNetwork, etc.
 
     from pybrain.tools.shortcuts import buildNetwork
     network = buildNetwork(NUM_REG_POS, HIDDEN_LAYER_SIZE, NUM_OUTPUTS,
-                     hiddenclass=SigmoidLayer, outclass=SigmoidLayer, recurrent=True)
+                     bias=True, hiddenclass=LSTMLayer, outclass=TanhLayer, recurrent=True)
 
-    network.randomize()
+    # buildNetwork() calls sortModules() at the end on its own
+
     return network
 
 
 # http://pybrain.org/docs/api/supervised/trainers.html
 # backprop's "through time" on a sequential dataset
 def train(network_module, training_data, testing_data, n=20):
-    ''' -- BPTT TRAINING ALGORITHM -- '''
     from pybrain.supervised.trainers import BackpropTrainer
-    trainer = BackpropTrainer(module=network_module, dataset=training_data)
+    trainer = BackpropTrainer(module=network_module, dataset=training_data, verbose=True)
 
     for i in range(n):
         trainer.trainEpochs(epochs=1)
         print 'epoch', i, 'finished'
 
-        # http://pybrain.org/docs/tutorial/fnn.html
-        train_result = percentError(
-            trainer.testOnClassData(),
-            train_data['class'])
+        # modified from testOnClassData source code
+        training_data.reset()
+        out = []
+        targ = []
+        num = 0
+        p = 0
+        for seq in training_data._provideSequences():
+            trainer.module.reset()
+            a = True
+            for inp, target in seq:
+                if a:
+                    t = target
+                    a = False
+                res = trainer.module.activate(inp)
+            #if p < 20:
+            #    print 'target:', t
+            #    print 'result:', res
+            #    p += 1
+            out.append(argmax(res))
+            targ.append(argmax(t))
+            if out[-1] == targ[-1]:
+                num += 1
+        p += 1
+        if p % 20 == 0:
+            print 'training error =', num, '/', training_data.getNumSequences()
 
-        test_result = percentError(
-            trainer.testOnClassData(dataset=testing_data),
-            test_data['class'])
-
-        print "epoch: %4d" % trainer.totalepochs, \
-            "  train error: %5.2f%%" % train_result, \
-            "  test error: %5.2f%%" % test_result
-
+        ## http://pybrain.org/docs/tutorial/fnn.html
+        #train_result = percentError(
+        #    trainer.testOnClassData(dataset=training_data, verbose=True),
+        #    train_data['class'])
+        #
+        #test_result = percentError(
+        #    trainer.testOnClassData(dataset=testing_data, verbose=True),
+        #    test_data['class'])
+        #
+        #print "epoch: %4d" % trainer.totalepochs, \
+        #    "  train error: %5.2f%%" % train_result, \
+        #    "  test error: %5.2f%%" % test_result
 
 
 if __name__ == "__main__":
     train_data, test_data = create_train_and_test_sets(MIN_LEN, MAX_LEN)
     network = build_sigmoid_network()
-    train(network_module=network, training_data=train_data, testing_data=test_data, n=10)
+    start = time.clock()
+    train(network_module=network, training_data=train_data, testing_data=test_data, n=1000)
+    print time.clock() - start, 'seconds'
