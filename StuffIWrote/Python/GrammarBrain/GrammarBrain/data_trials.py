b7cd923de8c4f8cfa669409db3cb54568ac1308d
ethanp <ethanp@utexas.edu>
2013-11-15 10:53:50 -0600

    RNN-G: make sure datasets are being created correctly

--- StuffIWrote/Python/GrammarBrain/GrammarBrain/data_trials.py
+++ StuffIWrote/Python/GrammarBrain/GrammarBrain/data_trials.py
@@ -1,7 +1,13 @@
 import grammar_trainer
 
 if __name__ == "__main__":
-    train_data, test_data = grammar_trainer.create_train_and_test_sets(MIN_LEN=5, MAX_LEN=8)
-    print train_data.getSequence(0)
-    print train_data.getSequence(1)
+    train_data, test_data = grammar_trainer.create_train_and_test_sets(MIN_LEN=5, MAX_LEN=12)
+    #print train_data.getSequence(0)
+    #print train_data.getSequence(1)
+    print train_data.getSequenceLength(0)
+    print train_data.getSequenceLength(1)
+    print train_data.getSequenceLength(2)
     print train_data.getSequenceLength(3)
+    print train_data.getSequenceLength(4)
+    print train_data.getSequenceLength(5)
+    print train_data.getSequenceLength(6)
