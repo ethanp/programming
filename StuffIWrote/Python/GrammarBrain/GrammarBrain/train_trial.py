0624d3574f3af87751473fe6163853b8f17d818f
ethanp <ethanp@utexas.edu>
2013-11-14 23:41:27 -0600

    RNN-G: sample set is NOT working, I didn’t change anything though!

--- StuffIWrote/Python/GrammarBrain/GrammarBrain/train_trial.py
+++ StuffIWrote/Python/GrammarBrain/GrammarBrain/train_trial.py
@@ -15,6 +15,11 @@ from pybrain.structure.modules import TanhLayer, LSTMLayer
 
 sds = SequenceClassificationDataSet(3, 1)
 
+# TODO make this echo my dataset, by having one bit on in each vector at a time
+# e.g. make it learn
+#               NOUN --> VERB ==> OK!
+#               ADJ  --> ADJ  ==> NOPE!
+
 sds.appendLinked([0,0,0], [0])
 sds.appendLinked([0,0,0], [0])
 
@@ -41,10 +46,19 @@ print sds['target'] # now it's a (2 by n array)
 
 # Build a recurrent Network.
 
+# bias and outputbias are on by default. I can't think of any good reason to turn them off
+# I suppose it's situation-specific, and in my situation, I just want to learn the function
+#   without any constraints on what that crazy function's intercept values are
+# bias adds a "biasModule" on all the hidden layers
+#   and if outputbias is True too, then also on the output layer
 rnet = buildNetwork(3, 3, 2, hiddenclass=LSTMLayer, outclass=TanhLayer, recurrent=True)
 
+
+# does this help, and why?
 recCon = FullConnection(rnet['out'], rnet['hidden0'])
 rnet.addRecurrentConnection(recCon)
+
+# must re-sort after adding another connection
 rnet.sortModules()
 
 print "------Before Training:"
