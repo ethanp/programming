cfdd181d63af715519f58aaa24d8b6e412cf845c
ethanp <ethanp@utexas.edu>
2013-11-23 13:21:42 -0600

    RNN-G: multi-proc manually to better see training progress

--- StuffIWrote/Python/GrammarBrain/GrammarBrain/Experiments.py
+++ StuffIWrote/Python/GrammarBrain/GrammarBrain/Experiments.py
@@ -22,58 +22,50 @@ def test_experiment_setup():
     #gt.timed_train()
     #gt.make_csv()
 
-def one_big_first_go():
-    '''
-    test the effect of including numbers and/or punctuation
-    '''
+
+''' test the effect of including numbers and/or punctuation '''
+
+def both():
     both_dict = default_dict.copy()
     both_dict['title'] = 'First Real Experiment'
     both_dict['part'] = '1'
     both_dict['maxim'] = 7
     both_dict['train_time'] = 40
+    both_gt = BrownGrammarTrainer(**both_dict)
+    both_gt.timed_train(s=4)
 
-    num_dict = both_dict.copy()
-    punct_dict = both_dict.copy()
-    none_dict = both_dict.copy()
-
-    pid1 = os.fork()
-    if pid1 == 0:
-        both_gt = BrownGrammarTrainer(**both_dict)
-        both_gt.timed_train(s=4)
-        exit(1)
-
-    num_dict['include_punctuation'] = False
+def no_punct():
+    num_dict = default_dict.copy()
+    num_dict['title'] = 'First Real Experiment'
     num_dict['part'] = '2'
+    num_dict['maxim'] = 7
+    num_dict['train_time'] = 40
+    num_dict['include_punctuation'] = False
+    num_gt = BrownGrammarTrainer(**num_dict)
+    num_gt.timed_train(s=4)
 
-    pid2 = os.fork()
-    if pid2 == 0:
-        num_gt = BrownGrammarTrainer(**num_dict)
-        num_gt.timed_train(s=4)
-        exit(2)
-
-
-    punct_dict['include_numbers'] = False
+def no_num():
+    punct_dict = default_dict.copy()
+    punct_dict['title'] = 'First Real Experiment'
     punct_dict['part'] = '3'
-    pid3 = os.fork()
-    if pid3 == 0:
-        punct_gt = BrownGrammarTrainer(**punct_dict)
-        punct_gt.timed_train(s=4)
-        exit(3)
+    punct_dict['maxim'] = 7
+    punct_dict['train_time'] = 40
+    punct_dict['include_numbers'] = False
+    punct_gt = BrownGrammarTrainer(**punct_dict)
+    punct_gt.timed_train(s=4)
 
+def none():
+    none_dict = default_dict.copy()
+    none_dict['title'] = 'First Real Experiment'
+    none_dict['part'] = '4'
+    none_dict['maxim'] = 7
+    none_dict['train_time'] = 40
     none_dict['include_punctuation'] = False
     none_dict['include_numbers'] = False
-    none_dict['part'] = '4'
 
-    pid4 = os.fork()
-    if pid4 == 0:
-        none_gt = BrownGrammarTrainer(**none_dict)
-        none_gt.timed_train(s=4)
-        exit(4)
+    none_gt = BrownGrammarTrainer(**none_dict)
+    none_gt.timed_train(s=4)
 
-    a = os.waitpid(pid1, 0)
-    b = os.waitpid(pid2, 0)
-    c = os.waitpid(pid3, 0)
-    d = os.waitpid(pid4, 0)
 
 if __name__ == '__main__':
-    one_big_first_go()
+    none()
