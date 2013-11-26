0c54f05f187c66b4127427e973ff5f57e7bdc0c2
ethanp <ethanp@utexas.edu>
2013-11-25 21:44:17 -0600

    RNN-G: histogram of ALL brown sentences s.t. len = 3..23

--- StuffIWrote/Python/GrammarBrain/GrammarBrain/brown_data/experiment_scripts/sentence_length_histogram.py
+++ StuffIWrote/Python/GrammarBrain/GrammarBrain/brown_data/experiment_scripts/sentence_length_histogram.py
@@ -1,6 +1,8 @@
 import cPickle as pickle
 import csv
 import GrammarBrain.brown_data.util.get_brown_pos_sents as gbp
+from collections import Counter
+
 
 def double_filter_sentence_length_histogram():
     with open('../experiment_results/sentence_length_histogram.csv', 'wb') as out_file:
@@ -18,5 +20,16 @@ def filter_numbers_sentence_length_histogram():
             print i-1, len(sentences)
             writer.writerow([i-1, len(sentences)])
 
+def no_filters_sentence_length_histogram():
+    with open('../experiment_results/raw_sentence_length_histogram.csv', 'wb') as out_file:
+        writer = csv.writer(out_file)
+        ss = gbp.get_brown_tagged_sentence_tuples(3,24)
+        c = Counter()
+        for s in ss:
+            c[len(s)] += 1
+        print c
+        for row in sorted(c.items()):
+            writer.writerow(row)
+
 if __name__ == '__main__':
-    filter_numbers_sentence_length_histogram()
+    no_filters_sentence_length_histogram()
