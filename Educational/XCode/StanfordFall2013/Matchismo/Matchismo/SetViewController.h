b95c0a61fcbf51c27207677d19833c7b1165be83
ethanp <ethanp@utexas.edu>
2013-12-05 17:53:32 -0600

    iOS-tut Assn3: (UNSURE…) make SetViewController INHERIT from CardGameViewController

---
+++ Educational/XCode/StanfordFall2013/Matchismo/Matchismo/SetViewController.h
@@ -0,0 +1,13 @@
+//
+//  SetViewController.h
+//  Matchismo
+//
+//  Created by Ethan Petuchowski on 12/5/13.
+//  Copyright (c) 2013 Ethan Petuchowski. All rights reserved.
+//
+
+#import "CardGameViewController.h"
+
+@interface SetViewController : CardGameViewController
+
+@end
---
+++ Educational/XCode/StanfordFall2013/Matchismo/Matchismo/SetViewController.m
@@ -0,0 +1,45 @@
+//
+//  SetViewController.m
+//  Matchismo
+//
+//  Created by Ethan Petuchowski on 12/5/13.
+//  Copyright (c) 2013 Ethan Petuchowski. All rights reserved.
+//
+
+#import "SetViewController.h"
+
+
+/**
+ This INHERITS from CardGameViewController()
+ I'm not sure if that's what's /supposed/ to go on
+ Actually, at this point, I'm not even sure what that DOES!
+ */
+
+@interface SetViewController ()
+
+@end
+
+@implementation SetViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
