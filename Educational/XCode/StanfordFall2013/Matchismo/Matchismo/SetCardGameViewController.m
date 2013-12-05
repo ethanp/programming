4db23fb597d994de2e686456a42ee876caa9d9c1
ethanp <ethanp@utexas.edu>
2013-12-05 17:56:59 -0600

    iOS-tut Assn3, Set: connect score and redeal buttons

--- Educational/XCode/StanfordFall2013/Matchismo/Matchismo/SetCardGameViewController.m
+++ Educational/XCode/StanfordFall2013/Matchismo/Matchismo/SetCardGameViewController.m
@@ -31,10 +31,5 @@
 	// Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
