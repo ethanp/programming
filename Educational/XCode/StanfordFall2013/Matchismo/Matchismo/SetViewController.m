182e2b29ad4c66c9e429a657821b66bde0bce20d
ethanp <ethanp@utexas.edu>
2013-12-05 22:15:38 -0600

    iOS-tut Assn3, Set: document abstraction issue

--- Educational/XCode/StanfordFall2013/Matchismo/Matchismo/SetViewController.m
+++ Educational/XCode/StanfordFall2013/Matchismo/Matchismo/SetViewController.m
@@ -9,10 +9,23 @@
 #import "SetViewController.h"
 
 
-/**
+/*
  This INHERITS from CardGameViewController()
  I'm not sure if that's what's /supposed/ to go on
- Actually, at this point, I'm not even sure what that DOES!
+ At this point, I'm not even sure what that DOES in the case of view controllers!
+ 
+ I'm having the following problem:
+    I want to 'connect' the SetCards of the view to a @property *cardCollection
+    but since the *cardCollection is in the SuperClass, 
+        I can't just drag it into this window
+    So instead I have connected the SetCards to the *cardCollection in the
+        SuperClass.m file, because that makes the /most/ sense out of my options
+    but it still may be incorrect, and may even not work once things start going.
+ 
+ The only solution is to wait until there is enough implementation there
+    to see whether it did what I intended or not.
+ I can't find anyone explicitly referencing this issue in a way I can understand
+    on StackOverflow
  */
 
 @interface SetViewController ()
