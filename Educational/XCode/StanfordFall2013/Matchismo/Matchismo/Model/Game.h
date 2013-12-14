04b26cd38c9293636e0a48347c62c2e2da0ce746
ethanp <ethanp@utexas.edu>
2013-12-13 20:04:10 -0800

    iOS-tut Assn3: SetGame and CardMatchingGame will inherit from Game so they can share a lot of code

---
+++ Educational/XCode/StanfordFall2013/Matchismo/Matchismo/Model/Game.h
@@ -0,0 +1,13 @@
+//
+//  Game.h
+//  Matchismo
+//
+//  Created by Ethan Petuchowski on 12/13/13.
+//  Copyright (c) 2013 Ethan Petuchowski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Game : NSObject
+
+@end
