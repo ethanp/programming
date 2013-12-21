958cb7217ab89b87d7eecbd65b49c302be55f389
ethanp <ethanp@utexas.edu>
2013-12-20 23:42:18 -0800

    iOS-tut Assn4: layout (hopefully) better defined. NON WORKING CODE. In the middle of: properly laying out the cards according to the number in play

--- Educational/XCode/StanfordFall2013/DyanamicMatchismo/DyanamicMatchismo/BaseViewController.m
+++ Educational/XCode/StanfordFall2013/DyanamicMatchismo/DyanamicMatchismo/BaseViewController.m
@@ -31,47 +31,28 @@
     [self updateUI];
 }
 
-
 - (IBAction)touchRedealButton:(UIButton *)sender {
     [self restartGame];
 }
 
-- (IBAction)touchCardButton:(UIButton *)sender
+- (NSInteger)numCardsInPlay
 {
-    [self updateUI];
-}
-
-- (void)updateUI
-{
-
-    // count cards in play
     int numCardsInPlay = 0;
     for (Card *card in self.game.cards) {
         if (!card.chosen) {
             numCardsInPlay++;
         }
     }
-    
+    return numCardsInPlay;
+}
+
+- (void)updateUI
+{
     CGFloat height = self.layoutContainerView.bounds.size.height;
     CGFloat width  = self.layoutContainerView.bounds.size.width;
     [_grid setCellAspectRatio:height/width];
     [_grid setSize:CGSizeMake(height, width)];
-    [_grid setMinimumNumberOfCells:numCardsInPlay];
-
-    
-// ============================ THE OLD METHOD =============================
-//    for (UIButton *cardButton in self.cardButtons) {
-//        int cardButtonIndex = [self.cardButtons indexOfObject:cardButton];
-//        Card *card = [self.game cardAtIndex:cardButtonIndex];
-//        [cardButton setTitle:[self titleForCard:card]
-//                    forState:UIControlStateNormal];
-//        [cardButton setBackgroundImage:[self backgroundImageForCard:card]
-//                              forState:UIControlStateNormal];
-//        cardButton.titleLabel.font = [UIFont systemFontOfSize:14];
-//        cardButton.enabled = !card.isMatched;
-//        self.scoreLabel.text =
-//        [NSString stringWithFormat:@"Score: %d", self.game.score];
-//    }
+    [_grid setMinimumNumberOfCells:[self numCardsInPlay]];
 }
 
 - (NSString *)titleForCard:(Card *)card
