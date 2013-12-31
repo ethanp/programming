864a82cff10813011a96552eb45fbae3f1c03a38
Ethan Petuchowski <etahnp@gmail.com>
2013-12-31 15:41:56 -0800

    iOS-tut Assn5: drilled down into JSON to get name of first place in top places

--- Educational/XCode/StanfordFall2013/Top Places/Top Places/BrowseTabViewController.m
+++ Educational/XCode/StanfordFall2013/Top Places/Top Places/BrowseTabViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "BrowseTabViewController.h"
+#import "FlickrFetcher.h"
 
 @interface BrowseTabViewController ()
 
@@ -14,31 +15,24 @@
 
 @implementation BrowseTabViewController
 
-- (id)initWithStyle:(UITableViewStyle)style
-{
-    self = [super initWithStyle:style];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
- 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    NSData *data = [NSData dataWithContentsOfURL:[FlickrFetcher URLforTopPlaces]];
+    NSDictionary *jsonDict = [NSJSONSerialization JSONObjectWithData:data
+                                                             options:0 error:nil];
+    NSLog(@"%@", jsonDict);
+    NSDictionary *placesDict = jsonDict[@"places"];
+    NSLog(@"%@", placesDict);
+    NSArray *placeArray = placesDict[@"place"];
+    NSLog(@"%@", placeArray);
+    NSDictionary *firstPlace = placeArray[0];
+    NSLog(@"%@", firstPlace);
+    NSString *placeString = firstPlace[@"_content"];
+    NSLog(@"%@", placeString);
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 #pragma mark - Table view data source
 
@@ -56,10 +50,12 @@
     return 0;
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *CellIdentifier = @"Cell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier
+                                                            forIndexPath:indexPath];
     
     // Configure the cell...
     
@@ -67,48 +63,10 @@
 }
 
 /*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
 #pragma mark - Navigation
 
-// In a story board-based application, you will often want to do a little preparation before navigation
+// In a story board-based application,
+// you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
     // Get the new view controller using [segue destinationViewController].
