eaa6e49dfd2c03a91f98cadf63c0e31d0b485962
ethanp <ethanp@utexas.edu>
2014-03-08 16:02:28 -0800

    started fleshing out what exactly I'm doing

--- StuffIWrote/Scala/CommentAnalyzer/CommentCollector_0/src/main/scala/na/ethan/AkkaRetrieval/AkkaRetriever.scala
+++ StuffIWrote/Scala/CommentAnalyzer/CommentCollector_0/src/main/scala/na/ethan/AkkaRetrieval/AkkaRetriever.scala
@@ -1,12 +1,48 @@
+/**
+ * Ethan Petuchowski
+ * 3/8/14
+ *
+ * Goal:
+ * -----
+ * Efficiently download and analyze comments from YouTube
+ * by passing them between different actors that do different
+ * pieces of the whole pipeline.
+ *
+ * Problem this Addresses:
+ * -----------------------
+ * My app currently does the following:
+ *
+ *  1. Download next page of youtube comments (network)
+ *  2. Parse comments into components (CPU)
+ *  3. Calculate sentiment value of comments (CPU)
+ *  4. Store comments in database (I/O)
+ *  5. Repeat as necessary
+ *
+ * NOTE: Each repetition has to wait on the last, but this is for nought!
+ *
+ * I'd like to parallelize this by introducing actors which are each
+ * responsible for a different piece of the action, but I could have
+ * a bunch of them doing performing each task at the same time, with
+ * no hard limit as far as the program itself is concerned.
+ *
+ * Actors:
+ * -------
+ *  YouTube Retriever: downloads a given page of comments from YouTube
+ *  Youtube Comment Parser: turns a page of comments into a list of comment components
+ *      (still needs to be fleshed out)
+ *  GPlus Retriever: downloads a given page of comments from Google Plus
+ *  GPlus Comment Parser: turns a page of comments into a list of comment components
+ *  Sentiment Analyzer: returns the sentiment value of a given piece of text
+ */
+
 package na.ethan.AkkaRetrieval
 
 import akka.actor.{ActorSystem, ActorRef, Actor, Props}
 
 /**
- * Ethan Petuchowski
- * 3/8/14
+ * Downloads a given page of comments from youtube
  */
-class Retriever extends Actor {
+class YouTubeRetriever extends Actor {
     // TODO receive
     def receive: Actor.Receive = {
         case _ => None
@@ -14,12 +50,8 @@ class Retriever extends Actor {
 }
 
 object HelloGDataWithAkka extends App {
-
-    println("Hello World!")
-
-    val system = ActorSystem(name = "helloGData")
-
-    val retriever : ActorRef = system.actorOf(props=Props[Retriever], name="retriever")
-
+    println("First Line")
+    val system = ActorSystem(name="helloGData")
+    val retriever : ActorRef = system.actorOf(props=Props[YouTubeRetriever], name="retriever")
     println("Final Line")
 }
