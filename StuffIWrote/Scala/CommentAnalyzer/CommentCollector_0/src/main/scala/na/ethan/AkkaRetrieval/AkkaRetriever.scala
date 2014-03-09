3f51e6d36b2f5d09b590db43a4ca884c664ada86
ethanp <ethanp@utexas.edu>
2014-03-08 18:11:23 -0800

    CC: YouTubeCommentRetriever "tells" YouTubeCommentParser to print the downloaded comments

--- StuffIWrote/Scala/CommentAnalyzer/CommentCollector_0/src/main/scala/na/ethan/AkkaRetrieval/AkkaRetriever.scala
+++ StuffIWrote/Scala/CommentAnalyzer/CommentCollector_0/src/main/scala/na/ethan/AkkaRetrieval/AkkaRetriever.scala
@@ -24,23 +24,21 @@
  * responsible for a different piece of the action, but I could have
  * a bunch of them doing performing each task at the same time, with
  * no hard limit as far as the program itself is concerned.
- *
- * Actors:
- * -------
- *  YouTube Retriever: downloads a given page of comments from YouTube
- *  Youtube Comment Parser: turns a page of comments into a list of comment components
- *      (still needs to be fleshed out)
- *  GPlus Retriever: downloads a given page of comments from Google Plus
- *  GPlus Comment Parser: turns a page of comments into a list of comment components
- *  Sentiment Analyzer: returns the sentiment value of a given piece of text
  */
 
 package na.ethan.AkkaRetrieval
 
 import akka.actor._
+import com.google.gdata.client.youtube.{YouTubeQuery, YouTubeService}
+import java.net.URL
+import com.google.gdata.data.youtube.{CommentEntry, CommentFeed, VideoEntry}
+import scala.collection.JavaConverters._
 
-case class VideoToLookFor(id: String)
+case class IDToLookFor(id: String)
 case object RetrieveNextPage
+case class ParseYouTubeComments(comments: List[CommentEntry])
+
+// TODO create abstract base Retriever class
 
 /**
  * Downloads a given page of comments from youtube
@@ -48,11 +46,62 @@ case object RetrieveNextPage
 class YouTubeRetriever extends Actor {
     var video_id = ""
     var page_num = 0
+    val parser = context.actorOf(Props[YouTubeCommentParser], "YouTubeCommentParser")
+    val lines = scala.io.Source.fromFile("/etc/googleIDKey").mkString.split("\n")
+    val service : YouTubeService = new YouTubeService(lines(0), lines(1))
+
+    def getComments = {
+        val videoEntryUrl = "http://gdata.youtube.com/feeds/api/videos/" + video_id
+        val videoEntry = service.getEntry(new URL(videoEntryUrl), classOf[VideoEntry])
+        val comments = videoEntry.getComments
+        if (comments != null) {
+            val commentsUrl: String = comments.getFeedLink.getHref
+            println(commentsUrl)
+
+            // Get the comment feed use a new query
+            val youtubeQuery = new YouTubeQuery(new URL(commentsUrl))
+            val RESULTS_PER_PAGE = 50 // this is the max
+            youtubeQuery.setMaxResults(RESULTS_PER_PAGE)
+            youtubeQuery.setStartIndex(page_num * RESULTS_PER_PAGE + 1) // TODO this doesn't seem to work
+            val commentUrlFeed = youtubeQuery.getUrl
+            println("commentUrlFeed" + commentUrlFeed)
+            val commentFeed = service.getFeed(commentUrlFeed, classOf[CommentFeed])
+            val entriesList = commentFeed.getEntries.asScala.toList
+
+            commentFeed.getNextLink      // TODO use this to download the next time
+            commentFeed.getTotalResults  // TODO use this to decide whether there are more available
+
+            /*entriesList.asScala.foreach { c : CommentEntry =>*/
+            parser ! ParseYouTubeComments(entriesList)
+        }
+    }
 
     def receive: Actor.Receive = {
-        case VideoToLookFor(id) => {
+        case IDToLookFor(id) => {
             println(s"looking for: $id")
             video_id = id
+            page_num = 0
+        }
+        case RetrieveNextPage => {
+            println(s"retrieving page: $page_num")
+            getComments
+            page_num += 1
+        }
+    }
+}
+
+/**
+ * Downloads a given page of comments from Google Plus
+ */
+class GPlusRetriever extends Actor {
+    var gplus_id = ""
+    var page_num = 0
+    val parser = context.actorOf(Props[GPlusCommentParser], "GPlusCommentParser")
+
+    def receive: Actor.Receive = {
+        case IDToLookFor(id) => {
+            println(s"looking for: $id")
+            gplus_id = id
             page_num = 0
         }
         case RetrieveNextPage => {
@@ -62,11 +111,42 @@ class YouTubeRetriever extends Actor {
     }
 }
 
+/**
+ * Turns a page of comments into a list of comment components
+ */
+class YouTubeCommentParser extends Actor {
+    def receive = {
+        case ParseYouTubeComments(comments) => {
+            comments.foreach(c => println(c.getPlainTextContent))
+        }
+    }
+}
+
+/**
+ * Turns a page of comments into a list of comment components
+ */
+class GPlusCommentParser extends Actor {
+    def receive = {
+        case _ => ???
+    }
+}
+
+/**
+ * Returns the sentiment value of a given piece of text
+ */
+class SentimentAnalyzer extends Actor {
+    def receive = {
+        case _ => ???
+    }
+}
+
 object HelloGDataWithAkka extends App {
     println("First Line")
     val system = ActorSystem(name="helloGData")
     val retriever : ActorRef = system.actorOf(props=Props[YouTubeRetriever], name="retriever")
     val inbox = Inbox.create(system)
-    retriever ! VideoToLookFor("asdjf9")
+    retriever ! IDToLookFor("ADos_xW4_J0")  // <== "Intro to Google Data...etc."
+    retriever ! RetrieveNextPage
+    retriever ! RetrieveNextPage
     println("Final Line")
 }
