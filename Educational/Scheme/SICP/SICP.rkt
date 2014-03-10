35965d73403e41649cd1e1be582ffe41163370bb
ethanp <ethanp@utexas.edu>
2014-03-09 17:55:02 -0700

    SICP 1.16

--- Educational/Scheme/SICP/SICP.rkt
+++ Educational/Scheme/SICP/SICP.rkt
@@ -23,23 +23,43 @@
 
 ; iterative version
 (define (fI n)
-  
+
   ; goofy for-loop
   (define (iter-fI a b c n i)
     (if (> i n) c
-        (iter-fI b 
-                 c 
+        (iter-fI b
+                 c
                  (+ (* 3 a) (* 2 b) (* 1 c))
                  n
                  (+ i 1))))
-  
+
   (if (< n 3) n
       (iter-fI 1 2 4 n 4)))
-
-
 
 (fI 3)
 (fI 4)
 (fI 5)
 (fI 6)
 (fI 7)
+
+
+; 1.16
+
+(define (expI b n)
+  (cond ((= n 0) 1)
+        (iter-exp 1 b n)))
+
+(define (iter-exp a b n)
+  (cond ((= n 1) (* a b))
+        ((even? n) (iter-exp a (square b) (/ n 2)))
+        (else (iter-exp (* a b) b (- n 1)))))
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (square x) (* x x))
+
+(expI 1 0)
+(expI 2 4)
+(expI 2 10)
+(expI 5 3)
