fec09ab4c9683687ee1f5810b1a00ce97e0f2416
ethanp <ethanp@utexas.edu>
2014-03-29 02:22:22 -0700

    add textarea for submissions

--- StuffIWrote/Scala/Discussion-Center/app/actors/DiscussionActor.scala
+++ StuffIWrote/Scala/Discussion-Center/app/actors/DiscussionActor.scala
@@ -10,7 +10,15 @@ import akka.actor.Actor
 /**
  * The DiscussionActor manages a particular Discussion within the App.
  */
-
 class DiscussionActor extends Actor {
   def receive: Receive = ???
+}
+
+/**
+ * The DiscussionApp handles the boot-up and boot-down of the App.
+ * This means loading the Redis DB from disk and putting the
+ * data in the appropriate place, and whatever initializations are req'd.
+ */
+class DiscussionApp extends Actor {
+  def receive = ???
 }
--- StuffIWrote/Scala/Discussion-Center/app/controllers/Application.scala
+++ StuffIWrote/Scala/Discussion-Center/app/controllers/Application.scala
@@ -3,18 +3,23 @@ package controllers
 import play.api._
 import play.api.mvc._
 import play.api.libs.json.{Json, JsString, JsObject, JsValue}
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
 import play.api.libs.iteratee._
 import ExecutionContext.Implicits.global
-import play.api.libs.concurrent.Akka
 import scala.Some
 
 object Application extends Controller {
 
+  /**
+   * '/' was accessed
+   */
   def index = Action { implicit request =>
-    Ok(views.html.index(Some("This is the landing page")))
+    Ok(views.html.index(Some("Root URL")))
   }
 
+  /**
+   * Either a user is signed in, or redirect to the homepage
+   */
   def chatRoom(username: Option[String]) = Action { implicit request =>
     username match {
       case Some(string) => Ok(views.html.index(Some(string)))
@@ -22,6 +27,10 @@ object Application extends Controller {
     }
   }
 
+  /**
+   * This is where we define how to handle information coming
+   * in and out of the WebSocket
+   */
   def chatSocket(username: String) = WebSocket.using[JsValue] { implicit request =>
     println(s"\n$username sent the following message through the chatSocket:")
 
