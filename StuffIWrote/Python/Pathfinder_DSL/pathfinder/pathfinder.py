1fd2c42e775c080be22da2c92323ab15ecea288e
ethanp <ethanp@utexas.edu>
2014-03-31 23:34:20 -0700

    pathfinder: still needs to be tested

--- StuffIWrote/Python/Pathfinder_DSL/pathfinder/pathfinder.py
+++ StuffIWrote/Python/Pathfinder_DSL/pathfinder/pathfinder.py
@@ -5,15 +5,6 @@
 
 import os
 
-# 1st thing must be a path we can `cd into`
-#
-# next things follow the dsl:
-# * means any file
-# ^... means "startswith"
-# ...$ means "endswith"
-# ...= means "is_not_dir"
-from nltk import flatten
-
 def flatten(given):
     """
     Flatten a given list.
@@ -32,17 +23,32 @@ def test_flatten():
 
 
 def pathfinder(cd_able, path):
+    """
+    1st thing must be a path we can `cd into`
+
+    next things follow the DSL:
+        *    means any file
+        ^... means "startswith"
+        ...$ means "endswith"
+        ...= means "is_not_dir"
+    """
+    if len(path) == 0 or os.path.isdir(cd_able):
+        return [os.path.abspath(cd_able)]
+
     os.chdir(cd_able)
-    if len(path) == 0:  return [os.path.abspath(cd_able)]
     head = path[0]
     tail = path[1:]
-    starts_with, ends_with = False, False
+    starts_with, ends_with, is_file = False, False, False
 
     if head[0] == '*':
         return flatten([pathfinder(os.path.abspath('./'+f), tail) for f in os.listdir('.')])
 
     if head[0] == '^':
         starts_with = True
+        head = head[1:]
+
+    if head[-1] == '=':
+        is_file = True
         head = head[1:]
 
     if head[-1] == '$':
@@ -54,9 +60,11 @@ def pathfinder(cd_able, path):
 
     return flatten([pathfinder(os.path.abspath(p), tail)
                     for p in os.listdir('.')
-                    if p.startswith(starts_with)
-                    and p.endswith(ends_with)])
+                    if head in p
+                    and p.startswith(starts_with)
+                    and p.endswith(ends_with)
+                    and os.path.isdir(p) != is_file])
 
 
 if __name__ == '__main__':
-    test_flatten()
+    pathfinder(['/Users/ethan/Desktop', ['New Files', '*', '^Something', '*', '.csv$=']])
