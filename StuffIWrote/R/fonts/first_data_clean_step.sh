8c08084672c480a1fa7d47928e53afe1e087302f
ethanp <ethanp@utexas.edu>
2014-05-04 23:17:04 -0700

    remove "KPX" text clutter

--- StuffIWrote/R/fonts/first_data_clean_step.sh
+++ StuffIWrote/R/fonts/first_data_clean_step.sh
@@ -4,14 +4,14 @@
 # afm -> values
 
 inDir=~/Downloads/afm\ 2
-cd "$inDir" # need quotes bc of the space
+cd "$inDir"
 
 outDir=~/Downloads/afm_k
 mkdir -p $outDir
 
 ls | while read f; do
     newFile=$outDir/"$f"
-    grep "^KPX" "$f" > "$newFile"
+    grep "^KPX" "$f" | sed -E s'|KPX(\ )*||' > "$newFile"
     if [ $(wc -l < "$newFile") -eq "0" ]; then
         rm -f "$newFile"
     fi
--- StuffIWrote/R/fonts/import_afm.R
+++ StuffIWrote/R/fonts/import_afm.R
@@ -5,8 +5,7 @@ frames <- list()
 
 for (file in files) {
     a <- read.delim(file, header = FALSE, sep = " ", 
-                    colClasses = c("character","character", 
-                                   "character", "numeric"))
+                    colClasses = c("character", "character", "numeric"))
 
     # TODO I think this combines the data.frames into one
     # list of columns, but I want to keep them separate
