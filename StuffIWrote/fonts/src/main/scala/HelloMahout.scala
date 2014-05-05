3433b22e7172c337c26e1b309de78859e4db29c8
ethanp <ethanp@utexas.edu>
2014-05-05 02:24:41 -0700

    more (unfinished) data preprocessing with scala

--- StuffIWrote/fonts/src/main/scala/HelloMahout.scala
+++ StuffIWrote/fonts/src/main/scala/HelloMahout.scala
@@ -14,13 +14,17 @@ import scala.io.Source
 /**
  * Base Source:
  * fady64
- * https://github.com/fady64/com.rukbysoft.examples.mahout/blob/MahoutRecommender/src/main/java/com/rukbysoft/examples/mahout/recommender/MovieRecommender.java
+ * https://github.com/fady64/com.rukbysoft.examples.mahout/blob/MahoutRecommender/
+ * src/main/java/com/rukbysoft/examples/mahout/recommender/MovieRecommender.java
  */
 
 object A extends App {
 
   val model: DataModel = new FileDataModel(new File(
     "/Users/Ethan/Dropbox/CSyStuff/ProgrammingGit/StuffIWrote/fonts/src/main/scala/MoviesList.csv"
+
+    // Doesn't work yet because it's expecting (usr,mov,val) but I'm giving (usr,pt1,pt2,val)
+//    "/Users/Ethan/Dropbox/CSyStuff/ProgrammingGit/StuffIWrote/fonts/OutFileData.csv"
   ))
 
   val similarity: UserSimilarity = new PearsonCorrelationSimilarity(model)
@@ -38,8 +42,22 @@ object CleanData extends App {
   val dirName = "/Users/Ethan/Downloads/afm 2"
   val files = new java.io.File(dirName).listFiles.filter(_.getName.endsWith(".afm"))
   val writer = new PrintWriter("OutFileData.csv")
-  for ((file, i) <- files.zipWithIndex)
-    Source.fromFile(file).getLines().filter(_.startsWith("KPX"))
-      .map( i + _.replaceFirst("KPX","").replaceAll(" ",",") + "\n" ) foreach writer.write
-  writer.close()
+  files.map(Source.fromFile(_).getLines())
+  val files_data = files.zipWithIndex map { case (file, i) =>
+    val lines = Source.fromFile(file).getLines()
+    val correctLines = lines.filter(_ startsWith "KPX")
+    correctLines.map(i + _.replaceFirst("KPX", "").replaceAll(" ", ",") + "\n").toList
+  }
+  val data = files_data.flatten
+  val keys = data.map(s => {
+    val p = s.split(",")
+    s"${p(1)},${p(2)}"
+  })
+  val map = keys.groupBy {
+    case i => i
+  }
+  println(map.toString())
+
+//    formattedLines foreach writer.write
+//  writer.close()
 }
