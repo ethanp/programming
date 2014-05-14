0f0fa70ae21acb10d8a606db7707237966f1c38a
Ethan Petuchowski <etahnp@gmail.com>
2014-05-13 20:04:57 -0700

    coursera eda: assn#2 plot#6, and submitted

--- Educational/R/Coursera/Exploratory Data Analysis/Assn2/plot6.R
+++ Educational/R/Coursera/Exploratory Data Analysis/Assn2/plot6.R
@@ -14,8 +14,33 @@
 
 setwd('/Users/ethan/code/non_apple/programming/Educational/R/Coursera/Exploratory Data Analysis/Assn2')
 
+if (!exists("LA_data"))
+    LA_data <- NEI[NEI$fips == "06037",]
+
+# the year is a factor variable, so it's just a line to sum it up
+if (!exists("LA_by_year"))
+    LA_by_year <- aggregate(LA_data$Emissions, by=list(year=LA_data$year), FUN=sum)
+
+
 plot6 <- function() {
     # do the same as plot2 for LA County
     # plot them both on the same plot
     # add a label showing the different differences over time between the cities
+    
+    merged <- merge(LA_by_year, balt_by_year, by='year')
+    merged <- rename(merged, c('x.y' = 'Baltimore City', 'x.x' = 'Los Angeles County'))
+    
+    library(reshape)
+    dat <- melt(merged, id.vars = 'year')
+    
+    
+    q <- ggplot(data = dat, aes(year, value, color = variable))
+    q <- q + geom_point()
+    q <- q + scale_color_manual(values = c('red', 'blue'))
+    q <- q + opts(title = 'Emissions changes in Baltimore and LA compared')
+    q <- q + ylab(expression('PM2.5 Emissions (tons)'))
+    q <- q + geom_smooth(method = 'lm', fill=NA)
+    q <- q + coord_cartesian(ylim = c(0, 50000))
+    q
+    ggsave(file="plot6.png")
 }
