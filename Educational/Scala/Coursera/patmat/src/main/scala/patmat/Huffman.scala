f4d0031bd0223ce8dde689a8817cb1ff42b431c6
ethanp <ethanp@utexas.edu>
2014-05-18 20:34:46 -0700

    huffman: weight and chars

--- Educational/Scala/Coursera/patmat/src/main/scala/patmat/Huffman.scala
+++ Educational/Scala/Coursera/patmat/src/main/scala/patmat/Huffman.scala
@@ -26,9 +26,15 @@ object Huffman {
 
   // Part 1: Basics
 
-  def weight(tree: CodeTree): Int = ??? // tree match ...
+  def weight(tree: CodeTree): Int = tree match {
+    case Fork(l, r, c, w) => w
+    case Leaf(c, w) => w
+  }
 
-  def chars(tree: CodeTree): List[Char] = ??? // tree match ...
+  def chars(tree: CodeTree): List[Char] = tree match {
+    case Fork(l, r, c, w) => c
+    case Leaf(c, w) => List(c)
+  }
 
   def makeCodeTree(left: CodeTree, right: CodeTree) =
     Fork(left, right, chars(left) ::: chars(right), weight(left) + weight(right))
