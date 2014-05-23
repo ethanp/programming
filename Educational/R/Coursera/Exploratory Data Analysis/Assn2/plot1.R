ed8c26f15407965d0484a847c5fc2ac4a5bfd074
Ethan Petuchowski <etahnp@gmail.com>
2014-05-23 11:12:22 -0700

    eda 2: new version of plot 1

--- Educational/R/Coursera/Exploratory Data Analysis/Assn2/plot1.R
+++ Educational/R/Coursera/Exploratory Data Analysis/Assn2/plot1.R
@@ -15,23 +15,22 @@
 
 setwd('/Users/ethan/code/non_apple/programming/Educational/R/Coursera/Exploratory Data Analysis/Assn2')
 
+# the year is a factor variable, so it's just a line to sum it up
+if (!exists("emm_by_year"))
+    emm_by_year <- aggregate(NEI$Emissions, by=list(year=NEI$year), FUN=sum)
+
 # scatterplot of summed output for each year for which we have data
 plot1 <- function() {
     
-    # the year is a factor variable, so it's just a line to sum it up
-    if (!exists("emm_by_year"))
-        emm_by_year <- aggregate(NEI$Emissions, by=list(year=NEI$year), FUN=sum)
-    
-    png('plot1.png') # open PNG device
+
+    png('plot1B.png') # open PNG device
     
     # now scatter plot emmissions by year
-    plot(emm_by_year, ylim=c(0, 7.5E6), ylab='PM2.5 emmisions (tons)',
-         main='Total US PM2.5 emmisions 1999 to 2008', xaxt='n')
-    axis(1, at = emm_by_year$year)
-    
-    # and add a linear model
-    fit <- lm(emm_by_year$x ~ emm_by_year$year)
-    abline(fit)
+    barplot( as.matrix( t(emm_by_year) ), 
+             ylim = c(0, 7.5E6), 
+             ylab = 'PM2.5 emmisions (tons)',
+             main = 'Total US PM2.5 emmisions 1999 to 2008', 
+             names = t(emm_by_year$year))
     
     dev.off() # save PNG to filesystem
 }
