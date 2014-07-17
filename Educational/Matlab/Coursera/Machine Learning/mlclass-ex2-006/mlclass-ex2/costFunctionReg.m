446b915f288579a0e4fc63529fd27bd35423c65f
Ethan Petuchowski <etahnp@gmail.com>
2014-07-17 12:44:08 -0700

    crsa ML: asn 2 done

--- Educational/Matlab/Coursera/Machine Learning/mlclass-ex2-006/mlclass-ex2/costFunctionReg.m
+++ Educational/Matlab/Coursera/Machine Learning/mlclass-ex2-006/mlclass-ex2/costFunctionReg.m
@@ -17,10 +17,29 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+% old cost code
+for i = 1:m
+  J += -y(i)*log(sigmoid(theta'*X(i,:)'))-(1-y(i))*log(1-sigmoid(theta'*X(i,:)'));
+end
+J /= m;
 
+% add regularization [avoiding theta(1)]
+for j = 2:size(theta)
+  J += lambda * theta(j)^2 / (2*m);
+end
 
+% old gradient code
+for j = 1:size(grad)
+  for i = 1:m
+    grad(j) += (sigmoid(theta'*X(i,:)')-y(i))*X(i,j);
+  end
+  grad(j) /= m;
+end
 
-
+% add regularization
+for j = 2:size(grad)
+  grad(j) += lambda * theta(j) / m;
+end
 
 % =============================================================
 
