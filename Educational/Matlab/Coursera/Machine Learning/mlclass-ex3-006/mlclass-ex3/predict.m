8a5069e7f256e2e0af9215b3ba086225eab16256
Ethan Petuchowski <etahnp@gmail.com>
2014-07-17 18:21:39 -0700

    crsa ml hw 3: Neural net feedforward

--- Educational/Matlab/Coursera/Machine Learning/mlclass-ex3-006/mlclass-ex3/predict.m
+++ Educational/Matlab/Coursera/Machine Learning/mlclass-ex3-006/mlclass-ex3/predict.m
@@ -11,23 +11,21 @@ num_labels = size(Theta2, 1);
 p = zeros(size(X, 1), 1);
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
-%               vector containing labels between 1 to num_labels.
-%
-% Hint: The max function might come in useful. In particular, the max
-%       function can also return the index of the max element, for more
-%       information see 'help max'. If your examples are in rows, then, you
-%       can use max(A, [], 2) to obtain the max for each row.
+% Instructions: Set p to a vector containing labels.
+% Hint: max(A, [], 2) to obtain the max for each row.
 %
 
+% Add ones to the X data matrix
+X = [ones(m, 1) X];
 
+% matrix of Activations
+% row = Example
+% col = Neuron in layer
+a2 = sigmoid(X*Theta1');
+a2 = [ones(m, 1) a2];
+a3 = sigmoid(a2*Theta2');
 
-
-
-
-
-
+[blah, p] = max(a3, [], 2);
 
 % =========================================================================
 
