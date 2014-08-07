d41a15510b1d26bb5e8f9eabce9a575caeb03775
ethanp <ethanp@utexas.edu>
2014-08-06 19:07:33 -0500

    crsa ml hw5 RegLinReg learning curve

--- Educational/Matlab/Coursera/Machine Learning/mlclass-ex5-006/mlclass-ex5/learningCurve.m
+++ Educational/Matlab/Coursera/Machine Learning/mlclass-ex5-006/mlclass-ex5/learningCurve.m
@@ -54,6 +54,11 @@ error_val   = zeros(m, 1);
 % ---------------------- MY CODE ----------------------
 
 
+for i = 1:m
+    theta = trainLinearReg(X(1:i,:), y(1:i), lambda);
+    [error_train(i) grad] = linearRegCostFunction(X(1:i,:), y(1:i), theta, 0);
+    [error_val(i) grad] = linearRegCostFunction(Xval, yval, theta, 0);
+end
 
 % -------------------------------------------------------------
 
