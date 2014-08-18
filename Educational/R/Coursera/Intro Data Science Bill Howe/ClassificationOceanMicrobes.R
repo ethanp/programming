e13940648260ef0ae9750fd183a3dd18428c82cb
ethanp <ethanp@utexas.edu>
2014-08-18 16:14:02 -0500

    decision tree in R

--- Educational/R/Coursera/Intro Data Science Bill Howe/ClassificationOceanMicrobes.R
+++ Educational/R/Coursera/Intro Data Science Bill Howe/ClassificationOceanMicrobes.R
@@ -17,19 +17,46 @@ library(e1071)
 library(ggplot2)
 
 
-# Step 1
+########################
+# Step 1 (load and initial data inspection)
 data <- read.csv('/Users/Ethan/code/Coursera/datasci_course_materials/assignment5/seaflow_21min.csv')
 summary(data)
 
-# Step 2
+
+########################
+# Step 2 (separate train and test data)
 # based on http://topepo.github.io/caret/splitting.html
+# Note: p = proportion of data in the training set
 trainIndices <- createDataPartition(data$file_id, p=.8, list=FALSE, times=1)
 dataTrain <- data[ trainIndices,]
 dataTest  <- data[-trainIndices,]
 mean(dataTrain$time)
 
-# Step 3
+
+########################
+# Step 3 (grouped scatterplot)
 ggplot(dataTrain, aes(pe, chl_small, color=pop)) + geom_point()
 
-# Step 4
-# TODO
+
+########################
+# Step 4 (train a Decision Tree)
+
+# first we must create a "formula" object
+fol <- formula(pop ~ fsc_small + fsc_perp + fsc_big + pe + chl_big + chl_small)
+
+# "Recursive Partitioning and Regression Trees", this method "fits" the model
+model <- rpart(fol, method="class", data=dataTrain)
+
+print(model)
+
+
+########################
+# Step 5 (evaluate Decision Tree on test data)
+preds <- predict(model, newdata=dataTest, type="class")
+ab <- preds == dataTest$pop
+accuracy <- sum(ab) / length(ab)
+
+
+########################
+# Step 6 (Random Forest)
+# TDOOD
