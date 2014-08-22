65cd9b71efec4991b0807803ef739e76525b48b0
ethanp <ethanp@utexas.edu>
2014-08-22 12:58:06 -0500

    client gets Alice in Wonderland printed to screen. So basically, first mission accomplished.

--- StuffIWrote/NodeJS/CoffeeScript/TextProc/routes/index.js
+++ StuffIWrote/NodeJS/CoffeeScript/TextProc/routes/index.js
@@ -2,23 +2,31 @@ var express = require('express');
 var router = express.Router();
 var http = require('http');
 
-var send_book_to_client = function(text) {
-  console.log(text);
-}
-
 /* GET home page. */
 router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/alice', function(req, res) {
+
   var url = 'http://www.gutenberg.org/cache/epub/11/pg11.txt';
+
+  var send_book_to_client = function(text) {
+    res.send(text);
+  }
+
   http.get(url, function(res) {
     var body = '';
-    res.on('data', function(chunk){ body += chunk });
-    res.on('end', function(){ send_book_to_client(body) })
 
-    // TODO use AJAX to fire this back to the client
-    // then have some client-side JS to do the functionality I want.
+    res.on('data', function(chunk) {
+      body += chunk;
+    });
+
+    res.on('end', function() {
+      send_book_to_client(body);
+    })
 
   });
-  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
