8a68eace97f3a48c419f39f3d20dd0b5a45fb859
ethanp <ethanp@utexas.edu>
2014-09-22 20:58:15 -0500

    sched: fairness experiment done, that's all she wrote

--- Educational/C++/Scheduling/lab1_multi_fair.cc
+++ Educational/C++/Scheduling/lab1_multi_fair.cc
@@ -9,9 +9,9 @@
 #include <sys/wait.h>
 #include <sched.h>
 #include <unistd.h>
+#include <sys/sysinfo.h>/* get_nprocs */
 #include "bg.h"
 #include <ctime>        /* clock, clock_t, CLOCKS_PER_SEC */
-#include <libcgroup.h>
 
 #define FOUR_KB 1 << 12
 
@@ -67,25 +67,16 @@ int hash_stuff(void *buffer_loc)
 
 int main(int argc, char const *argv[])
 {
-    /* get desired number of hash procs */
     printf("Beginning...\n");
-    int hash_procs = 0;
-    if (argc >= 2) {
-        hash_procs = atoi(argv[1]);
-        if (hash_procs < 1) {
-            printf("Must have at least one processor\n");
-            exit(EXIT_FAILURE);
-        }
-    } else {
-        printf("Enter number of processors\n");
-        exit(EXIT_FAILURE);
-    }
-    printf("%d procs requested\n", hash_procs);
+
+    /* number of hash procs is determined by number of processors found */
+    int hash_procs = get_nprocs();
+    printf("%d available logical processors found\n", hash_procs);
 
     /* get desired number of bg procs */
     int bg_procs = 0;
-    if (argc >= 3) {
-        bg_procs = atoi(argv[2]);
+    if (argc >= 2) {
+        bg_procs = atoi(argv[1]);
         printf("using %d bg procs\n", bg_procs);
     } else {
         printf("defaulting to 0 bg procs\n");
@@ -105,11 +96,6 @@ int main(int argc, char const *argv[])
     }
     urandom_file.read(reinterpret_cast<char*>(buffer_to_hash), FOUR_KB);
 
-    int ret = cgroup_init();
-    if (ret) {
-        printf("cgroup_init failed with '%d': %s\n", ret, cgroup_strerror(ret));
-        exit(EXIT_FAILURE);
-    }
     /* prep for hash procs */
     char *stacks[hash_procs];
     pid_t pids[hash_procs];
@@ -117,39 +103,28 @@ int main(int argc, char const *argv[])
     if (bg_procs > 0)
         start(bg_procs);  // start bg procs
 
-    /* create clone procs for hashing */
+    /* create clone procs for hashing & set the affinity of each */
     for (int proc = 0; proc < hash_procs; proc++) {
-        int err;
         stacks[proc] = (char*) malloc(STACK_SIZE);
         if (stacks[proc] == NULL) {
             printf("malloc failed\n");
             exit(EXIT_FAILURE);
         }
         pids[proc] = clone(hash_stuff, stacks[proc]+STACK_SIZE, CLONE_VM, buffer_to_hash);
-
-        /* assign hash procs to highest weight cpu-share cgroup */
-        // this did not work when all tasks were added to the same cgroup
-        struct cgroup *faster_group;
-        struct cgroup_controller *controller;
-        const int64_t HIGH_WEIGHT = 2048;
-        char* group_name = (char*)malloc(10);
-        sprintf(group_name, "faster%d", proc);
-        faster_group = cgroup_new_cgroup(group_name);
-        if (!faster_group) {
-            printf("cannot create 'faster' cgroup\n");
-            exit(EXIT_FAILURE);
-        }
-        controller = cgroup_add_controller(faster_group, "cpu");
-        cgroup_add_value_int64(controller, "cpu.shares", HIGH_WEIGHT);
-        int64_t upcasted_pid = (int64_t)pids[proc];
-        cgroup_add_value_int64(controller, "tasks", upcasted_pid);
-        err = cgroup_create_cgroup(faster_group, 0); // don't ignore errors
-        if (err) {
-            printf("create_cgroup failed with '%d': %s\n", err, cgroup_strerror(err));
+        cpu_set_t my_set;
+        CPU_ZERO(&my_set);
+        CPU_SET(0, &my_set);
+        if (sched_setaffinity(pids[proc], sizeof(cpu_set_t), &my_set) != 0) {
+            printf("problem with setaffinity for proc #%d, exiting\n", proc);
             exit(EXIT_FAILURE);
         }
     }
 
+    // start global timer
+    clock_t  start;
+    double   seconds, diff_ms;
+    start = clock();
+    
     ready = 1; // hashing should start now
 
     /* don't stop the bg procs until all the hashing procs have been reaped */
@@ -161,7 +136,6 @@ int main(int argc, char const *argv[])
             printf("problem with hash thread occurred\n");
         }
     }
-
 
     if (bg_procs > 0)
         stop();  // stop the bg procs
