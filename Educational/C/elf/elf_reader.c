010c9ac8a7dc1d3509a3d19a32e2b4746e33cd7b
ethanp <ethanp@utexas.edu>
2014-10-24 14:51:56 -0500

    pager: segfault on seg load

--- Educational/C/elf/elf_reader.c
+++ Educational/C/elf/elf_reader.c
@@ -69,10 +69,10 @@ int main(int argc, const char *argv[])
     /* mmap(the whole executable) */
     char* exec_addr;
     exec_addr = mmap(
-        NULL,           /* don't care where it goes in VAS */
-        filesize,       /* len */
-        PROT_READ,      /* pages may be read */
-        MAP_PRIVATE,    /* copy on write (shouldn't matter => it's prot_read */
+        (void*)0x800000, /* diff then where it's supposed to be (the pt) */
+        filesize,       /* length of image */
+        PROT_READ,      /* pages may be read but not written or exec'd */
+        MAP_PRIVATE | MAP_FIXED, /* COW (shouldn't matter => it's prot_read) */
         fd,             /* the file opened above */
         0               /* start at the very beginning (good place to start) */
     );
@@ -86,6 +86,9 @@ int main(int argc, const char *argv[])
 
     /* ensure valid elf version number */
     assert(hdr_p->e_version == EV_CURRENT);
+
+    /* ensure it's an executable file */
+    assert(hdr_p->e_type == ET_EXEC);
 
     printf("Entry point: 0x%x\n", (unsigned int)hdr_p->e_entry);
 
@@ -103,21 +106,42 @@ int main(int argc, const char *argv[])
 
         /* load it if it's a LOAD segment */
         if (is_load) {
-            printf(
+            printf( /* relevant LOAD segment metadata: */
                 "offset in file:    0x%x\n"
                 "virt addr:         0x%lx\n"
-                "phys addr:         0x%lx\n"
+                /*"phys addr:         0x%lx\n" (we use virt addr instead) */
                 "size in file:      0x%x\n"
                 "size in RAM:       0x%x\n"
                 "flags:             0x%x\n"
                 "align constraint:  0x%x\n\n",
-                (uint32_t)ph->p_offset,
-                (uint64_t)ph->p_vaddr,
-                (uint64_t)ph->p_paddr,
-                (uint32_t)ph->p_filesz,
-                (uint32_t)ph->p_memsz,
-                (uint32_t)ph->p_flags,
-                (uint32_t)ph->p_align);
+                (uint32_t) ph->p_offset,
+                (uint64_t) ph->p_vaddr,
+                /*(uint64_t) ph->p_paddr, (only for systems without virt mem) */
+                (uint32_t) ph->p_filesz,
+                (uint32_t) ph->p_memsz,
+                (uint32_t) ph->p_flags,
+                (uint32_t) ph->p_align);
+
+            /* copy file segment from filedesc to virtual memory segment */
+
+            int mmap_prot = ((ph->p_flags & PF_X) ? PROT_EXEC  : 0)
+                          | ((ph->p_flags & PF_W) ? PROT_WRITE : 0)
+                          | ((ph->p_flags & PF_R) ? PROT_READ  : 0);
+
+            char* seg_addr = mmap(
+                (void*) ph->p_vaddr,
+                ph->p_filesz, /* NOTE: this is just how long the raw data is
+                                 the in-RAM segment is bigger, so TODO I'll mmap
+                                 another chunk above this one?? */
+                mmap_prot,
+                MAP_PRIVATE | MAP_FIXED /* HAS to mmap to loc I request */,
+                fd,
+                ph->p_offset
+            );
+
+            if ( seg_addr == (char*)(-1) ) {
+                print_error("mmap seg_addr");
+            }
         }
     }
 
