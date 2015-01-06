4b80d6b7e1a020238684b5b2c9d449b9c6e4f3b4
ethanp <ethanp@utexas.edu>
2015-01-06 11:58:08 -0500

    JavaFX 8 - marco jakob tutorial pt 1

---
+++ Educational/Java/JavaFX/MarcoJakobTutorial/readme.md
@@ -0,0 +1,3 @@
+### A Very Thorough JavaFX 8 Tutorial
+
+Find it at [http://code.makery.ch/java/javafx-8-tutorial-part1/]()
---
+++ Educational/Java/JavaFX/MarcoJakobTutorial/src/address/MainApp.java
@@ -0,0 +1,61 @@
+package address;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApp extends Application {
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("AddressApp");
+
+        initRootLayout();
+
+        showPersonOverview();
+    }
+
+    public void initRootLayout() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+            rootLayout = loader.load();
+
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void showPersonOverview() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
+            AnchorPane personOverview = loader.load();
+            rootLayout.setCenter(personOverview);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
