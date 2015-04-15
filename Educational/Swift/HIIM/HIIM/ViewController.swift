0d20c2696393dff561fc5e2bb1873313021d1822
ethanp <ethanp@utexas.edu>
2015-04-14 20:47:49 -0500

    basic functionality is all there

--- Educational/Swift/HIIM/HIIM/ViewController.swift
+++ Educational/Swift/HIIM/HIIM/ViewController.swift
@@ -10,19 +10,17 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
-    let tipCalc = SessionModel()
     
+    @IBOutlet var timerLabel: UILabel!
+    @IBOutlet var summaryText: UITextView!
+    
+    var session = SessionModel()
+    let EVERY_SECOND: Double = 1
     var bellSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Bell", ofType: "m4a")!)
-    var audioPlayer = AVAudioPlayer()
-    
 
-    @IBAction func startSession(sender: AnyObject) {
-        // audio will play to completion once, even if you keep mashing it
-        audioPlayer.play()
-    }
-    
-    func refreshUI() {
-    }
+    var audioPlayer = AVAudioPlayer()
+    var timer = NSTimer()
+    var counter: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,7 +28,45 @@ class ViewController: UIViewController {
         audioPlayer = AVAudioPlayer(contentsOfURL: bellSound, error: nil)
         audioPlayer.prepareToPlay()
         
+        
+        
         refreshUI()
     }
+
+    func refreshUI() {
+    }
+    
+    /******** BUTTONS *********/
+    @IBAction func startSession(sender: AnyObject) {
+        session = SessionModel()
+        summaryText.text = ""
+        
+        // audio will play to completion once, even if you keep mashing it
+        audioPlayer.play()
+        timerLabel.text = String(counter)
+        timer = NSTimer.scheduledTimerWithTimeInterval(
+            EVERY_SECOND,
+            target: self,
+            selector: Selector("tick"),
+            userInfo: nil,
+            repeats: true
+        )
+    }
+    
+    
+    @IBAction func endSession(sender: AnyObject) {
+        timer.invalidate()
+        
+        summaryText.text = "Your session lasted \(counter-1) seconds"
+        counter = 0
+    }
+    
+    
+    /******* util *******/
+    func tick() {
+        timerLabel.text = String(counter++)
+        
+    }
+
 }
 
