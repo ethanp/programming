7777db51ededa874fae39a975a34653c5ea00bf0
ethanp <ethanp@utexas.edu>
2015-09-02 04:08:52 -0500

    persistent timer - base files

---
+++ Educational/Swift/PersistentTimer/PersistentTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PersistentTimer
+//
+//  Created by Ethan Petuchowski on 9/2/15.
+//  Copyright (c) 2015 Ethanp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
