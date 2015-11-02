e0c305be061fa5931a3ec5141141e9701410a9e7
ethanp <ethanp@utexas.edu>
2015-11-02 14:32:24 -0600

    move http2 experiments to its own repo

--- StuffIWrote/Java/jetty-http2-comparison/src/main/java/ethanp/examples/FromStove.java
+++
@@ -1,9 +0,0 @@
-package ethanp.examples;
-
-/**
- * Ethan Petuchowski 10/23/15
- *
- * http://stackoverflow.com/questions/31031563/jetty-9-3-ssl-alpn-http2-error-err-empty-response-with-https-only
- */
-public class FromStove {
-}
