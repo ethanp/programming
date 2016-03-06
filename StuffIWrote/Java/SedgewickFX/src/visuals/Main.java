da0d730e6461f82b6f9ad6052f8ccacd7852af3e
ethanp <ethanp@utexas.edu>
2016-03-05 18:22:16 -0800

    simple bar graphing is working

--- StuffIWrote/Java/SedgewickFX/src/visuals/Main.java
+++ StuffIWrote/Java/SedgewickFX/src/visuals/Main.java
@@ -9,34 +9,13 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import util.Pair;
 
 public class Main extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-
     static GraphicsContext gc;
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Hello World");
-        StackPane root = new StackPane();
-        Scene s = new Scene(root, 400, 400, Color.BLACK);
-        final Canvas canvas = new Canvas(300, 300);
-        gc = canvas.getGraphicsContext2D();
-        clearCanvas();
-        root.getChildren().addAll(canvas);
-        ObservableList<Pair<Double>> data = FXCollections.observableArrayList(
-            new Pair<>(3.0, 4.0),
-            new Pair<>(1.0, 3.0)
-        );
-        NonNegativeLineGraph lineGraph = new NonNegativeLineGraph(gc);
-        lineGraph.drawBasedOn(data);
-        primaryStage.setScene(s);
-        primaryStage.show();
-        primaryStage.show();
+    public static void main(String[] args) {
+        launch(args);
     }
 
     public static void drawStuff() {
@@ -47,5 +26,25 @@ public class Main extends Application {
     public static void clearCanvas() {
         gc.setFill(Color.BLACK);
         gc.fillRect(0, 0, gc.getCanvas().getHeight(), gc.getCanvas().getWidth());
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Hello World");
+        StackPane root = new StackPane();
+        Scene s = new Scene(root, 400, 400, Color.BLACK);
+        final Canvas canvas = new Canvas(400, 400);
+        gc = canvas.getGraphicsContext2D();
+        clearCanvas();
+        root.getChildren().addAll(canvas);
+        ObservableList<Double> data = FXCollections.observableArrayList(
+            4.0,
+            3.0, 5.0, 2.0
+        );
+        NonNegativeBarGraph lineGraph = new NonNegativeBarGraph(gc);
+        lineGraph.drawBasedOn(data);
+        primaryStage.setScene(s);
+        primaryStage.show();
+        primaryStage.show();
     }
 }
---
+++ StuffIWrote/Java/SedgewickFX/src/visuals/NonNegativeBarGraph.java
@@ -0,0 +1,96 @@
+package visuals;
+
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import util.Pair;
+
+import java.util.ListIterator;
+
+/**
+ * Ethan Petuchowski 3/5/16
+ */
+public class NonNegativeBarGraph {
+    final GraphicsContext gc;
+    final double totalHeight;
+    final double totalWidth;
+    final double graphHeight;
+    final double graphWidth;
+    final double graphProportion = .8;
+    private ObservableList<Double> values = null;
+    private double xScale = 0;
+    private double yScale = 0;
+
+    public NonNegativeBarGraph(GraphicsContext gc) {
+        this.gc = gc;
+        this.totalHeight = gc.getCanvas().getHeight();
+        this.totalWidth = gc.getCanvas().getWidth();
+        this.graphHeight = totalHeight*graphProportion;
+        this.graphWidth = totalWidth*graphProportion;
+    }
+
+    public void drawBasedOn(ObservableList<Double> values) {
+        this.values = values;
+        this.values.addListener((ListChangeListener<? super Double>) c -> redraw());
+        redraw();
+    }
+
+    public double maxValue() { return values.stream().max(Double::compare).get(); }
+
+    private void setXScale() { xScale = graphWidth/values.size(); }
+
+    private void setVerticalScale() { yScale = graphHeight/maxValue(); }
+
+    private void redraw() {
+        Main.clearCanvas();
+        drawAxisLines();
+        setXScale();
+        setVerticalScale();
+        drawBars();
+    }
+
+    private void drawBars() {
+        ListIterator<Double> it = values.listIterator();
+        gc.setLineWidth(30);
+        int idx = 0;
+        while (it.hasNext()) {
+            idx++;
+            double barX = idx*xScale+getLeftSide()-10;
+            double val = it.next();
+            double barY = getBottomSide()-val*yScale;
+            gc.setStroke(Color.BLUE);
+            gc.strokeLine(barX, getBottomSide(), barX, barY);
+        }
+    }
+
+    private void drawAxisLines() {
+        gc.setStroke(Color.RED);
+        gc.setLineWidth(3);
+        Pair<Double> lowerLeftCorner = getLowerLeftCorner();
+        Pair<Double> lowerRightCorner = getLowerRightCorner();
+        Pair<Double> topLeftCorner = getTopLeftCorner();
+        gc.strokeLine(lowerLeftCorner.a, lowerLeftCorner.b, lowerRightCorner.a, lowerRightCorner.b);
+        gc.strokeLine(lowerLeftCorner.a, lowerLeftCorner.b, topLeftCorner.a, topLeftCorner.b);
+    }
+
+    private Double getLeftSide() { return (totalWidth-graphWidth)/2; }
+
+    private Double getRightSide() { return (totalWidth+graphWidth)/2; }
+
+    private Double getTopSide() { return (totalHeight-graphHeight)/2; }
+
+    private Double getBottomSide() { return (totalHeight+graphHeight)/2; }
+
+    private Pair<Double> getLowerLeftCorner() {
+        return new Pair<>(getLeftSide(), getBottomSide());
+    }
+
+    private Pair<Double> getLowerRightCorner() {
+        return new Pair<>(getRightSide(), getBottomSide());
+    }
+
+    private Pair<Double> getTopLeftCorner() {
+        return new Pair<>(getLeftSide(), getTopSide());
+    }
+}
