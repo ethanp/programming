8791d2c8820c8157aba11429d2400209029ab8e1
ethanp <ethanp@utexas.edu>
2016-06-26 02:36:42 -0700

    es6 is actually not difficult to just try out

---
+++ Educational/Javascript/intellij-plain/javascripts.js
@@ -0,0 +1,32 @@
+var earth = new Image()
+function init() {
+    window.requestAnimationFrame(draw)
+}
+
+const TreeNode = (value) => {
+    return {
+        draw: () => {
+            return value
+        }
+    }
+}
+
+const root = TreeNode(6)
+console.log(root.draw())
+
+function draw() {
+    const ctx = document.getElementById('canvas').getContext('2d')
+    // Sets all pixels in the rectangle defined by
+    // starting point (x, y) and size (width, height)
+    // to transparent black, erasing any previously drawn content.
+    ctx.clearRect(0, 0, 300, 300)
+    ctx.beginPath()
+
+    // void ctx.arc(x, y, radius, startAngle (radians), endAngle, anticlockwise);
+    ctx.arc(150, 150, 105, 0, Math.PI * 2, false)
+    ctx.stroke()
+
+    // start animation loop (we're not currently using it though,
+    // so no further rendering will happen)
+    window.requestAnimationFrame(draw)
+}
