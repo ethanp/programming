d4da53b06dcf7b16dd510abb10156a8a860617b1
ethanp <ethanp@utexas.edu>
2016-07-24 20:45:03 -0700

    javascript works again

--- Educational/Javascript/intellij-plain/index.js
+++ Educational/Javascript/intellij-plain/index.js
@@ -1,32 +1,40 @@
-const Tree = (treeNodes) => {
-    return {
-        treeBelow: () => {
-
-        }
-    }
-}
-
-const TreeNode = (value, p, pc, ch, con) => {
-    const id = value
-    const parent = p
-    const relativePath = pc
-    const children = ch || [{
+const TreeNode = (node_id, node_parents, node_pathComponent, node_children, node_content) => {
+    const id = node_id
+    const parents = node_parents || [{ nodeId: "asdf" }, { nodeId: "bteg" }]
+    const pathComponent = node_pathComponent
+    const children = node_children || [{
             isSymbolic: true,
             nodeId:     "asdf"
         }]
-    const content = con || {}
+    const content = node_content || {}
     return {
         draw: () => {
-            return value
+            return node_id
         }
     }
 }
 
-const root = TreeNode(6, null, "")
-console.log(root.draw())
+const VisualNode = (xCoord, yCoord, baseNode) => {
+    const x = xCoord
+    const y = yCoord
+    const treeNode = baseNode
+    return {
+        draw: () => {
+            return x
+        }
+    }
+}
+
+var nodes = []
+const clickHandler = (evt) => {
+    console.log(evt.x, evt.y)
+    nodes.push(VisualNode(evt.x, evt.y, TreeNode("asdf")))
+    console.log(nodes.map((node) => node.draw()))
+}
 
 function draw() {
     const ctx = document.getElementById('canvas').getContext('2d')
+    document.getElementById('body').onclick = clickHandler
     // Sets all pixels in the rectangle defined by
     // starting point (x, y) and size (width, height)
     // to transparent black, erasing any previously drawn content.
