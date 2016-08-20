576c833f383507bd7c0bf3e7438f908ebd7b119b
ethanp <ethanp@utexas.edu>
2016-08-20 11:11:10 -0700

    switched to class syntax, now it's working

--- StuffIWrote/Javascript/FileSystemTree/FileSystemTree.js
+++ StuffIWrote/Javascript/FileSystemTree/FileSystemTree.js
@@ -1,69 +1,99 @@
-const pathObj = (str) => {
-    let value = str
-    return {
-        parent: () => value.substring(0, value.lastIndexOf("/")),
-        basename: () => value.substring(value.lastIndexOf("/") + 1, value.length)
+class pathObj {
+    constructor(str) {
+        this.value = str
+    }
+
+    parent() {
+        return this.value.substring(0, this.value.lastIndexOf("/"))
+    }
+
+    basename() {
+        return this.value.substring(this.value.lastIndexOf("/") + 1, this.value.length)
     }
 }
 
-const tree = () => {
-    const root = node("")
-    return {
-        getNode: (path) => {
-            return path.split("/") // get array of path components
-            .filter(s => s != "") // remove the root
-            .reduce( // travel down tree
-                (cur, next) => cur != null ? cur.getChild(next) : null,
-                root
-            )
-        },
-        /** returns the parent node*/
-        addNode: (node, parentPath) => {
-            if (parentPath == null || parentPath == "") return root.addChild(node)
-            const parent = this.getNode(parentPath)  // crashes here: this.getNode is not a function (wat?)
-            if (parent == null) return null
-            return parent.addChild(node)
-        },
-        deleteNode: (path) => {
-            const pathObj = pathObj(path)
-            const parentNode = this.getNode(pathObj.parent())
-            if (parentNode == null) return null
-            return parentNode.removeChild(pathObj.basename())
-        },
-        moveNode: (component, oldDir, newDir) => {
-            const node = oldDir.getChild(component)
-            oldDir.removeChild(component)
-            newDir.addChild(node)
-        },
-        treeString: () => root.treeString()
+class tree {
+    constructor() {
+        this.root = new node("")
+    }
+
+    getNode(path) {
+        return path.split("/") // get array of path components
+        .filter(s => s != "") // remove the root and any trailing slash
+        .reduce( // travel down tree
+            (cur, next) => cur != null ? cur.getChild(next) : null,
+            this.root
+        )
+    }
+
+    /** returns the parent node*/
+    addNode(node, parentPath) {
+        if (parentPath == null || parentPath == "") return this.root.addChild(node)
+        const parent = this.getNode(parentPath)  // crashes here: this.getNode is not a function (wat?)
+        if (parent == null) return null
+        return parent.addChild(node)
+    }
+
+    deleteNode(path) {
+        const pathObj = new pathObj(path)
+        const parentNode = this.getNode(pathObj.parent())
+        if (parentNode == null) return null
+        return parentNode.removeChild(pathObj.basename())
+    }
+
+    moveNode(component, oldDir, newDir) {
+        const oldParent = this.getNode(oldDir)
+        const node = oldParent.getChild(component)
+        oldParent.removeChild(component)
+        this.getNode(newDir).addChild(node)
+        return this
+    }
+
+    treeString() {
+        return this.root.treeString()
     }
 }
 
-const node = (pathPiece) => {
-    let children = []
-    let pathComponent = pathPiece
-    Array.prototype.flatMap = (lambda) => Array.prototype.concat.apply([], this.map(lambda))
-    return {
-        getPathComponent: () => pathComponent,
-        addChild: (child) => {
-            children.push(child)
-            return this
-        },
-        removeChild: (pathPiece) => {
-            const preLen = children.length
-            children = children.filter(c => c.getPathComponent() != pathPiece)
-            return preLen != children.length
-        },
-        getChild: (pathPiece) => children.find(c => c.getPathComponent() == pathPiece),
-        treeString: () => [pathComponent].concat(children.flatMap(c => c.treeString().map(line => "\t" + line)))
+class node {
+    constructor(pathPiece) {
+        this.children = []
+        this.pathComponent = pathPiece
+    }
+
+    getPathComponent() {
+        return this.pathComponent
+    }
+
+    addChild(child) {
+        this.children.push(child)
+        return this
+    }
+
+    removeChild(pathPiece) {
+        const preLen = this.children.length
+        this.children = this.children.filter(c => c.getPathComponent() != pathPiece)
+        return preLen != this.children.length
+    }
+
+    getChild(pathPiece) {
+        return this.children.find(c => c.getPathComponent() == pathPiece)
+    }
+
+    treeString() {
+        let first = [this.pathComponent]
+        // javascript has no "flatMap" so we do it by hand
+        for (var i = 0; i < this.children.length; i++) {
+            first = first.concat(this.children[i].treeString().map(l => "\t" + l))
+        }
+        return first
     }
 }
 
-const t = tree()
-const node1 = node("1st")
-const node2 = node("2nd")
-const node3 = node("3rd")
+const t = new tree()
+const node1 = new node("1st")
+const node2 = new node("2nd")
+const node3 = new node("3rd")
 t.addNode(node1, null)
 t.addNode(node2, null)
-t.addNode(node3, node2)
+t.addNode(node3, "/2nd")
 t.treeString().forEach(line => console.log(line))
