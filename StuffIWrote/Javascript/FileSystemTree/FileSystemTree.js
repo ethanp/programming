7ada475ad85494d97a7796b0158906a14d649efe
ethanp <ethanp@utexas.edu>
2016-08-20 13:57:16 -0700

    explore adding kefir

--- StuffIWrote/Javascript/FileSystemTree/FileSystemTree.js
+++ StuffIWrote/Javascript/FileSystemTree/FileSystemTree.js
@@ -1,3 +1,11 @@
+const kefir = require('kefir')
+const EventEmitter = require('events')
+
+/* TODO
+* make it interact with kefir
+* make it a node module e.g. in npm
+*/
+
 class pathObj {
     constructor(str) {
         this.value = str
@@ -31,21 +39,28 @@ class tree {
         if (parentPath == null || parentPath == "") return this.root.addChild(node)
         const parent = this.getNode(parentPath)
         if (parent == null) return null
-        return parent.addChild(node)
+        return {
+            success: parent.addChild(node),
+            tree: this
+        }
     }
 
     deleteNode(path) {
         const pathObj = new pathObj(path)
         const parentNode = this.getNode(pathObj.parent())
         if (parentNode == null) return null
-        return parentNode.removeChild(pathObj.basename())
+        return {
+            success: parentNode.removeChild(pathObj.basename()),
+            tree: this
+        }
     }
 
     moveNode(component, oldDir, newDir) {
         const oldParent = this.getNode(oldDir)
         const node = oldParent.getChild(component)
         oldParent.removeChild(component)
-        this.getNode(newDir).addChild(node)
+        const newParent = this.getNode(newDir)
+        newParent.addChild(node)
         return this
     }
 
@@ -59,6 +74,7 @@ class node {
         this.children = []
         this.parent = null
         this.pathComponent = pathPiece
+        this.data = new Map()
     }
 
     getPathComponent() {
@@ -109,5 +125,31 @@ const node3 = new node("3rd")
 t.addNode(node1, null)
 t.addNode(node2, null)
 t.addNode(node3, "/2nd")
-console.log(node3.getAbsolutePath())
-t.treeString().forEach(line => console.log(line))
+//console.log(node3.getAbsolutePath())
+//t.treeString().forEach(line => console.log(line))
+
+const t2 = new tree();
+["a", "b", "c"].forEach(p => t2.addNode(new node(p)))
+//console.log(t2.getNode("/").children.map(c => c.pathComponent))
+//t2.treeString().forEach(line => console.log(line))
+
+const em = new EventEmitter()
+
+var userCreatedConnection = kefir.fromEvents(em, "userCreatedConnection")
+
+em.on("userCreatedConnection", evt => console.log(evt + ": from em.on"))
+
+const mapped = userCreatedConnection.map(evt => {
+    console.log("asdf")
+    const n1 = evt.fromNode
+    const n2 = evt.toNode
+    //const result = t.addNode(n1, n2)
+    return "mapped"
+}).log()
+
+mapped.onValue(val => console.log("inside onValue"))
+
+userCreatedConnection.onValue((val) => console.log("got value"))
+
+em.emit("userCreatedConnection", "the string we emitted")
+
