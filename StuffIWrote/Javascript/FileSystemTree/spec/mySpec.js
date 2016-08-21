d81f366a97e9f33fbf25c848f597b41967f03306
ethanp <ethanp@utexas.edu>
2016-08-21 02:30:34 -0700

    implement first mocha test

---
+++ StuffIWrote/Javascript/FileSystemTree/spec/mySpec.js
@@ -0,0 +1,27 @@
+const tree = require('../FileSystemTree')
+const assert = require('assert')
+
+describe("a file system tree", () => {
+    it("spec name", () => {
+        console.log(tree)
+        return assert.equal(true, true)
+    })
+    it("should be creatable", () => {
+        return assert.equal(new tree(), "asdf");
+    })
+    it("spec name", () => {})
+    it("spec name", () => {})
+    it("spec name", () => {})
+    it("spec name", () => {})
+    it("spec name", () => {})
+    it("spec name", () => {})
+})
+
+describe("a file system node", () => {
+    it("something", () => {})
+    it("something", () => {})
+    it("something", () => {})
+    it("something", () => {})
+    it("something", () => {})
+    it("something", () => {})
+})
