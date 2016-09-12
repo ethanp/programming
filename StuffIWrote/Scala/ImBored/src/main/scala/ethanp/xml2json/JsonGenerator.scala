2795dcb23cea80577dc93877020cef804b780b01
ethanp <ethanp@utexas.edu>
2016-09-11 22:31:08 -0700

    deserialized json generator seems to work

--- StuffIWrote/Scala/ImBored/src/main/scala/ethanp/xml2json/JsonGenerator.scala
+++ StuffIWrote/Scala/ImBored/src/main/scala/ethanp/xml2json/JsonGenerator.scala
@@ -1,35 +1,24 @@
 package ethanp.xml2json
 
-import scala.collection.mutable
-
 /**
   * 9/11/16 8:47 PM
   */
 class JsonGenerator {
     def generateJson(rootAstNode: ASTNode): JsonObj = {
         rootAstNode match {
-            case elem: TextElem => return basicObjWithName(elem.text)
+            case elem: TextElem =>
+                JsonObj(Seq(JsonField("name", elem.text)))
             case elem: DOMElem =>
-                JsonObj(Seq(
-                    (JsonName("name"), JsonValue(JsonValueElem(elem.name)))
-                ) ++ elem.textAndChildren.map {
-                    // TODO this part is not quite correct yet
-                    case TextElem(name: String) =>
-                        (JsonName(name), JsonValue(JsonValueElem(name)))
-                    case x: DOMElem =>
-                        (JsonName("asdf"), JsonValue(JsonValueElem("asdf")))
-                    case x => throw new RuntimeException(
-                        s"unexpected thingy: $x"
-                    )
-                })
+                val nameField = JsonField("name", elem.name)
+                val childElems: Seq[Json] = elem.textAndChildren.map {
+                    case TextElem(name: String) => JsonName(name)
+                    case child: DOMElem => generateJson(child)
+                    case err => throw new RuntimeException(s"unexpected child elem: $err")
+                }
+                val childrenField = JsonField(JsonName("children"), JsonArr(childElems))
+                JsonObj(nameField :: childrenField :: Nil)
         }
-        JsonObj()
     }
-
-    def basicObjWithName(name: String) = JsonObj(Seq((
-        JsonName("name"),
-        JsonValue(JsonValueElem(name)))
-    ))
 }
 
 object JsonGenerator {
@@ -39,14 +28,14 @@ object JsonGenerator {
 
 sealed trait Json
 
-case class JsonObj(
-    // TODO it may not need to be mutable
-    pairs: Seq[(JsonName, JsonValue)] = mutable.ArrayBuffer.empty
-) extends Json
+case class JsonObj(pairs: Seq[JsonField]) extends Json
 
-case class JsonArr(arr: Seq[JsonValue]) extends Json
+case class JsonArr(arr: Seq[Json]) extends Json
 case class JsonName(name: String) extends Json
-case class JsonValue(json: Json) extends Json
-
+case class JsonField(jsonName: JsonName, jsonValue: Json) extends Json
+object JsonField {
+    def apply(a: String, b: Any): JsonField =
+        JsonField(JsonName(a), JsonValueElem(b))
+}
 /** could be a Double, Long, or String */
 case class JsonValueElem(valueElem: Any) extends Json
